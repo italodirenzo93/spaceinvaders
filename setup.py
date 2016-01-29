6cc97b26a8934738a184a5749a6cb5e187ebd58a
Italo Di Renzo <italodirenzo93@gmail.com>
2016-01-28 20:34:27 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
-from distutils.core import setup
-
-files = ['data/*']
+from setuptools import setup
 
 setup(
 	name = 'SpaceInvaders',
-	version = '0.0.0',
 	description = 'Its space invaders...',
 	author = 'Italo Di Renzo',
 	author_email = 'italodirenzo93@gmail.com',
-	url = '',
 	packages = ['spaceinvaders'],
-	package_data = {'package': files},
-	long_description = '',
+	package_data = {'spaceinvaders': ['data/*']},
+	install_requires = ['pygame'],
+	dependency_links = ['https://bitbucket.org/pygame/pygame'],
 )
